0dc0adddc9d2fcb4051746399bc212c85017eb4b
Ricardo Hermida Ruiz <ricardo.ruiz@locaweb.com.br>
2016-03-24 14:21:00 -0300

    cowboy only on test

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule HeartCheck.Mixfile do
     [
       {:poison, "~> 2.0"},
       {:plug, "~> 1.0"},
-      {:cowboy, "~> 1.0"},
+      {:cowboy, "~> 1.0", only: :test},
       {:ex_doc, "~> 0.11", only: :dev},
       {:earmark, "~> 0.1", only: :dev}
     ]
