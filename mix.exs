e5cdb3db7915aa4bd3d5085dc970a7074576b5c3
Ricardo Hermida Ruiz <ricardo.ruiz@locaweb.com.br>
2017-03-31 16:03:53 -0300

    Version bump

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule HeartCheck.Mixfile do
   use Mix.Project
 
   def project do [app: :heartcheck,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
