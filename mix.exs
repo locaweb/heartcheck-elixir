66be93585dbedf02e2264c57ef1dc71eb9f21166
Thiago Santos <thia.mdossantos@gmail.com>
2020-10-21 16:43:49 -0300

    Fix warnings

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule HeartCheck.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [extra_applications: [:logger]]
   end
 
   def package do
